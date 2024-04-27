9c257851e58805a3a8e38e5be8ba8f1246ea8ac7
mazen-ahmed <mazenreal22@gmail.com>
2024-04-27 20:41:55 +0300

    added local url in env

--- src/services/businesses.ts
+++ src/services/businesses.ts
@@ -1,7 +1,5 @@
-import axios from "axios";
-
 export const getBusiness = async (slug: string) => {
-	const response = await fetch(`http://localhost:3000/api/business/${slug}`);
+	const response = await fetch(`${process.env.LOCAL_URL}/businesses/${slug}`);
 
 	const { data } = await response.json();
 
