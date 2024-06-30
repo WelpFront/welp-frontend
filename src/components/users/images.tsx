40a58d6bdd04f40de0f21a73cc812a9b3d0ad891
mazen-ahmed <mazenreal22@gmail.com>
2024-06-30 14:51:46 +0300

    corrected image paths

--- src/components/users/images.tsx
+++ src/components/users/images.tsx
@@ -4,21 +4,21 @@ const ImagesSection = () => {
 	return (
 		<div className="relative -mt-56 flex items-center justify-center gap-10">
 			<Image
-				src="/iphone.svg"
+				src="/iPhone.svg"
 				width={200}
 				height={200}
 				alt="iphone"
 				className="hidden md:block"
 			/>
 			<Image
-				src="/iphone.svg"
+				src="/iPhone.svg"
 				width={250}
 				height={300}
 				alt="iphone"
 				className=""
 			/>
 			<Image
-				src="/iphone.svg"
+				src="/iPhone.svg"
 				width={200}
 				height={200}
 				alt="iphone"
