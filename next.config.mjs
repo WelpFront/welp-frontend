08c05d7dd65d3382bc2f65567feaa4e5e7d81aeb
mazen-ahmed <mazenreal22@gmail.com>
2024-07-10 20:00:43 +0300

    apple universal links handling

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,15 @@ import createNextIntlPlugin from  'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/:file',
+        destination: '/api/.well-known/:file',
+        permanent: false,
+      },
+    ]
+  },
     images: {
         remotePatterns: [{
           protocol: 'https',
