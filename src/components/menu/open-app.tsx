c4a1ec0dd37e2e954a91b6ff3ea296bd6aeae9ff
mazen-ahmed <mazenreal22@gmail.com>
2024-07-10 21:03:30 +0300

    test ios link

--- src/components/menu/open-app.tsx
+++ src/components/menu/open-app.tsx
@@ -1,4 +1,5 @@
-import { Link } from "navigation";
+"use client";
+
 import Image from "next/image";
 
 const OpenApp = () => {
@@ -6,11 +7,13 @@ const OpenApp = () => {
 		<div className="bg-gray-50 flex  mx-3 rounded-lg justify-between mt-4 px-2 py-4 text-white">
 			<Image src={"/logo.svg"} width={60} height={30} alt="logo" />
 
-			<Link
-				href={"YOUR_TEAM_ID.com.welp.welp://"}
+			<button
+				onClick={() =>
+					(window.location.href = "YOUR_TEAM_ID.com.welp.welp://")
+				}
 				className="bg-secondary p-2 px-4 rounded-full ">
 				فتح الأبليكيشن
-			</Link>
+			</button>
 		</div>
 	);
 };
