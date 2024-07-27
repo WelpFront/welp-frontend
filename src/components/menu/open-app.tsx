7b7bad101217fe252256e0ee669ecccaaef8cc19
mazen-ahmed <mazenreal22@gmail.com>
2024-07-27 20:00:09 +0300

    testing open app component

--- src/components/menu/open-app.tsx
+++ src/components/menu/open-app.tsx
@@ -1,15 +1,38 @@
 "use client";
 
 import Image from "next/image";
+import { useEffect } from "react";
 
 const OpenApp = () => {
+	const handleOpenApp = () => {
+		window.location.href = "welp://";
+	};
+
+	useEffect(() => {
+		const handleVisibilityChange = () => {
+			if (!document.hidden) {
+				window.location.href = process.env
+					.NEXT_PUBLIC_FRONTEND_URL as string;
+			}
+		};
+
+		document.addEventListener("visibilitychange", handleVisibilityChange);
+
+		return () => {
+			document.removeEventListener(
+				"visibilitychange",
+				handleVisibilityChange
+			);
+		};
+	}, []);
+
 	return (
 		<div className="bg-gray-50 flex  mx-3 rounded-lg justify-between mt-4 px-2 py-4 text-white">
 			<Image src={"/logo.svg"} width={60} height={30} alt="logo" />
 
 			<button
-				onClick={() => window.location.assign("welp://")}
-				className="bg-secondary p-2 px-4 rounded-full ">
+				onClick={handleOpenApp}
+				className="bg-secondary p-2 px-4 rounded-full">
 				فتح الأبليكيشن
 			</button>
 		</div>
