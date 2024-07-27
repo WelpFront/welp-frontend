9a636aa361bfc5c1967d68f67681e8638d9ba681
mazen-ahmed <mazenreal22@gmail.com>
2024-07-27 20:51:54 +0300

    testing open app

--- src/components/menu/open-app.tsx
+++ src/components/menu/open-app.tsx
@@ -4,28 +4,6 @@ import Image from "next/image";
 import { useEffect } from "react";
 
 const OpenApp = () => {
-	const handleOpenApp = () => {
-		window.location.href = "welp://";
-	};
-
-	useEffect(() => {
-		const handleVisibilityChange = () => {
-			if (!document.hidden) {
-				window.location.href = process.env
-					.NEXT_PUBLIC_FRONTEND_URL as string;
-			}
-		};
-
-		document.addEventListener("visibilitychange", handleVisibilityChange);
-
-		return () => {
-			document.removeEventListener(
-				"visibilitychange",
-				handleVisibilityChange
-			);
-		};
-	}, []);
-
 	return (
 		<div className="bg-gray-50 flex  mx-3 rounded-lg justify-between mt-4 px-2 py-4 text-white">
 			<Image src={"/logo.svg"} width={60} height={30} alt="logo" />
