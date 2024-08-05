aa95624795b3c94b3d554ea2081cbfede8fe3147
mazen-ahmed <mazenreal22@gmail.com>
2024-08-05 15:26:31 +0300

    env variables

--- src/config/api-configs.ts
+++ src/config/api-configs.ts
@@ -1,5 +1,7 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+import { env } from "next-runtime-env";
 
-export const clientBaseURL = process.env.NEXT_PUBLIC_CLIENT_URL;
+export const API_URL = env("NEXT_PUBLIC_API_URL");
+
+export const clientBaseURL = env("NEXT_PUBLIC_CLIENT_URL");
 
 export const appId = process.env.APP_ID;
