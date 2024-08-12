26c5bdc690cb069034666b4f785c80775d9af29d
mazen-ahmed <mazenreal22@gmail.com>
2024-08-12 20:37:12 +0300

    fixed homepage header

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -1,11 +1,13 @@
+import { SearchInput } from "atoms";
 import React from "react";
 
 const Header = ({ translation }: { translation: any }) => {
 	return (
 		<div className="header flex items-center justify-center">
-			<h1 className=" md:text-5xl text-center font-bold text-white text-[40px]">
+			<h1 className=" md:text-5xl text-center font-bold text-white text-[24px] md:text-[40px]">
 				{translation.bestPlatform}
 			</h1>
+			<div>{/* <SearchInput /> */}</div>
 		</div>
 	);
 };
