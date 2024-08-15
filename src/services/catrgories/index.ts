45c218ab1552fd7c9d319570aeedd9f1a69d8f4f
mazen-ahmed <mazenreal22@gmail.com>
2024-08-15 12:31:58 +0300

    env variables

--- src/services/catrgories/index.ts
+++ src/services/catrgories/index.ts
@@ -1,5 +1,17 @@
 import * as client from "fetch/client";
 
+// import * as server from "fetch/server";
+
+export const getCategoriesList = async (exclude_first_parent: boolean) => {
+	try {
+		// const { data } = await server.get(`utilities/categories`);
+
+		return {};
+	} catch (error) {
+		return Response.json({ error });
+	}
+};
+
 export const getCategory = async (id: string) => {
 	try {
 		const { data } = await client.get(`utilities/categories/${id}`);
